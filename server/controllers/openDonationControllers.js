bc549461a322d04be5fa5112658fba6019745c85
Ben-bo <bennyizkandar@gmail.com>
2021-06-26 11:54:08 +0800

    add feature update on openDonation

--- server/controllers/openDonationControllers.js
+++ server/controllers/openDonationControllers.js
@@ -20,15 +20,17 @@ donationController.create = async (req, res) => {
 };
 donationController.getAllByUser = async (req, res) => {
   try {
+    let status = 200;
+    let message = "OK";
     const userId = req.body.userId;
     const dataDonation = await openDonationModel.findAll({
       where: {
         userId,
       },
     });
-    res.status(200).send({
-      status: 200,
-      message: "OK",
+    res.status(status).send({
+      status: status,
+      message: message,
       data: dataDonation,
     });
   } catch (error) {
@@ -37,6 +39,32 @@ donationController.getAllByUser = async (req, res) => {
       status: 500,
       message: "Failed to get Data Donation",
       erorr: error,
+    });
+  }
+};
+donationController.update = async (req, res) => {
+  try {
+    let status = 200;
+    let message = "OK";
+    const openDonationId = req.params.openDonationId;
+    const update = await openDonationModel.update(
+      { ...req.body },
+      { where: { id: openDonationId } }
+    );
+    const dataOpenDonation = await openDonationModel.findAll({
+      where: { id: openDonationId },
+    });
+    res.status(status).send({
+      status,
+      message,
+      totalRowChanged: update,
+      data: dataOpenDonation,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      status: 500,
+      message: "failed Update",
     });
   }
 };
--- server/routes/openDonationRoutes.js
+++ server/routes/openDonationRoutes.js
@@ -4,10 +4,20 @@ const tokenVerify = require("../middleware/tokenOpenDonation");
 const donationController = require("../controllers/openDonationControllers");
 router.post(
   "/openDonation",
-  tokenVerify.tokenVerify,
   openDonationMiddleware.donationValidate,
+  tokenVerify.tokenVerify,
   donationController.create
 );
-router.get("/my", tokenVerify.tokenVerify, donationController.getAllByUser);
+router.put(
+  "/openDonation/update/:openDonationId",
+  openDonationMiddleware.donationValidate,
+  tokenVerify.tokenVerify,
+  donationController.update
+);
+router.get(
+  "/openDonation/my",
+  tokenVerify.tokenVerify,
+  donationController.getAllByUser
+);
 
 module.exports = router;
