e102a6f983e3cad48a4f2851670347da0687c0a1
Ariseno <astral.arie@gmail.com>
2021-06-28 23:05:50 +0700

    add feature donature

--- server/controllers/donatureControllers.js
+++ server/controllers/donatureControllers.js
@@ -1,25 +1,81 @@
-const { Donature } = require("../models");
+const { User, Donature, OpenDonation } = require("../models");
 
-const routes = {};
+const sequelize = require("sequelize");
+const method = {};
 
-routes.createDonature = async (req, res) => {
+// Create All Donation, "when someone donation"
+method.createDonature = async (req, res) => {
   try {
-    const donature = await Donature.create({
-      ...req.body,
+    const result = await Donature.create({ ...req.body });
+    res.status(200).json({
+      message: "Donation Successfull",
+      data: result,
     });
-    const donatureResult = {
-      statusCode: 200,
-      statusText: "Success",
-      message: "Donature has been create",
-      data: donature,
-    };
-    res.json(donatureResult);
-  } catch (err) {
+  } catch (error) {
     res.status(500).json({
-      statusText: "Internal Server Error",
-      message: err.message,
+      message: error.message,
     });
   }
 };
 
-module.exports = routes;
+// Get All Donation
+method.getAllDonature = async (req, res) => {
+  try {
+    const result = await Donature.findAll({
+      include: { model: User },
+    });
+    res.status(200).json({
+      message: "this is List of Donature",
+      data: result,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+//GET Donatur by Id
+method.getDonatureById = async (req, res) => {
+  try {
+    const result = await Donature.findAll({
+      where: { id: req.params.id },
+    });
+    res.status(200).json({
+      message: `this is List of Donature with id : ${req.params.id}`,
+      result: { result },
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+// GET My Donation
+method.getMyDonation = async (req, res) => {
+  try {
+    const { UserId, Type } = req.query;
+    const result = await Donature.findAll({
+      where: {
+        userId: UserId,
+        donationTypeId: Type,
+      },
+      attributes: ["quantity"],
+      include: {
+        model: OpenDonation,
+      },
+    });
+
+    res.status(200).json({
+      message: "Donasi saya",
+      data: result,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+module.exports = method;
--- server/routes/donatureRoutes.js
+++ server/routes/donatureRoutes.js
@@ -1,10 +1,20 @@
 const express = require("express");
 
 const router = express.Router();
+const {
+  createDonature,
+  getAllDonature,
+  getDonatureById,
+  getMyDonation,
+} = require("../controllers/donatureControllers");
 
-//const { checkUser } = require("../middleware/auth");
-const { createDonature } = require("../controllers/donatureControllers");
+const { tokenVerify } = require("../middleware/tokenDonature");
 
-router.post("/addDonature", createDonature);
+//========================================Donature==============================
+router.get("/myDonation", getMyDonation);
+router.get("/allDonature", getAllDonature);
+router.get("/donatureById/:id", getDonatureById);
+router.post("/createDonature", tokenVerify, createDonature);
 
+//========================================Export Router===============================
 module.exports = router;
