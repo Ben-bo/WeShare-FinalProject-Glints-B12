d401861a17d71520c755ce4b10a6c12449122473
nndra15 <nandra.stta15@gmail.com>
2021-06-28 23:43:29 +0700

    fix  routes openDonation

--- server/routes/openDonationRoutes.js
+++ server/routes/openDonationRoutes.js
@@ -3,30 +3,30 @@ const openDonationMiddleware = require("../middleware/openDonationMiddleware");
 const tokenVerify = require("../middleware/tokenOpenDonation");
 const donationController = require("../controllers/openDonationControllers");
 router.post(
-  "/api/openDonation/create",
+  "/openDonation/create",
   tokenVerify.tokenVerify,
   openDonationMiddleware.donationValidate,
   donationController.create
 );
 router.put(
-  "/api/openDonation/update/:openDonationId",
+  "/openDonation/update/:openDonationId",
   openDonationMiddleware.donationValidate,
   tokenVerify.tokenVerify,
   donationController.update
 );
 router.get(
-  "/api/openDonation/my",
+  "/openDonation/my",
   tokenVerify.tokenVerify,
   donationController.getAllByUser
 );
-router.get("/api/openDonation/", donationController.getAll);
+router.get("/openDonation/", donationController.getAll);
 router.get(
-  "/api/openDonation/detail/:openDonationId",
+  "/openDonation/detail/:openDonationId",
   tokenVerify.tokenVerify,
   donationController.getAllById
 );
 router.delete(
-  "/api/openDonation/delete/:openDonationId",
+  "/openDonation/delete/:openDonationId",
   tokenVerify.tokenVerify,
   donationController.delete
 );
