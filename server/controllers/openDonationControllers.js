38a72bb4793f7c26ddbf60a6276a3d0237fe3b40
nndra15 <nandra.stta15@gmail.com>
2021-07-22 20:22:00 +0700

    change struktur data openDonationController

--- server/controllers/openDonationControllers.js
+++ server/controllers/openDonationControllers.js
@@ -111,7 +111,7 @@ donationController.getAllById = async (req, res) => {
       //   { model: donatureModel, include: [userModel, informationModel] },
       // ],
        include: [
-        categoryModel,
+        userModel, categoryModel,
         { model: openDonationDetailsModel, include: [
         { model: donationTypeModel, include: [
         { model: informationModel,include: [
@@ -145,10 +145,17 @@ donationController.getAll = async (req, res) => {
     let status = 200;
     let message = "OK";
     const dataDonation = await openDonationModel.findAll({
+      // include: [
+      //   categoryModel,
+      //   { model: openDonationDetailsModel, include: [donationTypeModel] },
+      //   { model: donatureModel, include: [userModel, informationModel] },
+      // ],
       include: [
         categoryModel,
-        { model: openDonationDetailsModel, include: [donationTypeModel] },
-        { model: donatureModel, include: [userModel, informationModel] },
+        { model: openDonationDetailsModel, include: [
+        { model: donationTypeModel, include: [
+        { model: informationModel,include: [
+        { model: donatureModel, include: [userModel ]}] }] }] },
       ],
     });
     res.status(status).send({
